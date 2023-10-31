ffcbb6a6cc6db0c63bb194d94511f06073152d22
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-10-31 11:28:06 +0100

    Added a sign up form

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
-class Churchmembers{
-    constructor(name,location){
-       this._name = name
-       this.location = location
-    }
-    get name(){
-        return this._name
-    }
-    joinWelfare(){
-        console.log('contributor to warefare')
-    }
-    present(){
-        console.log('Present @ Church today')
+let submitBtn = document.querySelector("button");
+submitBtn.addEventListener("click", submit);
+
+function submit() {
+    let inputVal = document.getElementById("name").value;
+    let inputVal2 = document.getElementById("location").value;
+    let inputVal3 = document.getElementById("contact").value;
+    let inputVal4 = document.getElementById("marital_status").value;
+
+    class Churchmembers {
+        constructor(name) {
+            this.name = name;
+        }
+        PayWelfare() {
+            console.log("contributor to warefare");
+        }
+        present() {
+            console.log("Present @ Church today");
+        }
+        absent() {
+            console.log("absent @ church today");
+        }
     }
 
-    absent(){
-        console.log('absent @ church today')
+    class Memberdetails extends Churchmembers {
+        constructor(name, location, contact, maritalStatus) {
+            super(name);
+            this.location = location;
+            this.contact = contact;
+            this.maritalstatus = maritalStatus;
+        }
     }
+
+    let newComer = new Memberdetails(`${inputVal}`, `${inputVal2}`, `${inputVal3}`, `${inputVal4}`);
+
+    console.log(newComer);
+
+    localStorage.setItem(b,x)
+    localStorage.getItem(b)
 }
-
-
-class Member extends Churchmembers{
-    constructor(name,location,contact){
-        super(name)
-        this.location = location
-        this.contact = contact
-    }
-}
-
-
-
-
-
-
-let newComer = new Member('Akua Konadu','Accra Osu', '0549762969')
-console.log(newComer)
-
