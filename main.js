b698e06e38bcb9d0e8cb0ea101d2fb7f5b0b12fb
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-12-07 11:57:42 +0100

    adding server to the project

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-let submitBtn = document.querySelector("button");
-submitBtn.addEventListener("click", submit);
+// let submitBtn = document.querySelector("button");
+// submitBtn.addEventListener("click", submit);
 
-function submit() {
-    let inputVal = document.getElementById("name").value;
-    let inputVal2 = document.getElementById("location").value;
-    let inputVal3 = document.getElementById("contact").value;
-    let inputVal4 = document.getElementById("marital_status").value;
+// function submit() {
+//     let inputVal = document.getElementById("name").value;
+//     let inputVal2 = document.getElementById("location").value;
+//     let inputVal3 = document.getElementById("contact").value;
+//     let inputVal4 = document.getElementById("marital_status").value;
 
-    class Churchmembers {
-        constructor(name) {
-            this.name = name;
-        }
-        PayWelfare() {
-            console.log("contributor to warefare");
-        }
-        present() {
-            console.log("Present @ Church today");
-        }
-        absent() {
-            console.log("absent @ church today");
-        }
-    }
+//     class Churchmembers {
+//         constructor(name) {
+//             this.name = name;
+//         }
+//         PayWelfare() {
+//             console.log("contributor to warefare");
+//         }
+//         present() {
+//             console.log("Present @ Church today");
+//         }
+//         absent() {
+//             console.log("absent @ church today");
+//         }
+//     }
 
-    class Memberdetails extends Churchmembers {
-        constructor(name, location, contact, maritalStatus) {
-            super(name);
-            this.location = location;
-            this.contact = contact;
-            this.maritalstatus = maritalStatus;
-        }
-    }
+//     class Memberdetails extends Churchmembers {
+//         constructor(name, location, contact, maritalStatus) {
+//             super(name);
+//             this.location = location;
+//             this.contact = contact;
+//             this.maritalstatus = maritalStatus;
+//         }
+//     }
 
-    let newComer = new Memberdetails(`${inputVal}`, `${inputVal2}`, `${inputVal3}`, `${inputVal4}`);
+//     let newComer = new Memberdetails(`${inputVal}`, `${inputVal2}`, `${inputVal3}`, `${inputVal4}`);
 
-    console.log(newComer);
+//     console.log(newComer);
 
-    localStorage.setItem(b,x)
-    localStorage.getItem(b)
-}
+//     localStorage.setItem(b, x);
+//     localStorage.getItem(b);
+// }
